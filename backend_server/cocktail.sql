a00025e4a7daa1e88ecae5f28dbfae7df15d0611
povermanr <rachel.poverman@gmail.com>
2024-02-22 17:31:49 -0500

    had to update to not version 18.17.0

--- backend_server/cocktail.sql
+++
@@ -1,8 +0,0 @@
-\echo 'Delete and recreate cocktail_app db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
-
-DROP DATABASE IF EXISTS cocktail_app;
-CREATE DATABASE cocktail_app;
-\connect cocktail_app
-
-\i Cocktail_DB.sql
--- backend_server/config.js
+++
@@ -1,5 +0,0 @@
-const BCRYPT_WORK_FACTOR = 10;
-
-module.exports = {
-  BCRYPT_WORK_FACTOR,
-};
